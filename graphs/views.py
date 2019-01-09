0267c86e9e3f690b8eda688c11202534ed594342
Tom McNally <tom_mcnally17@hotmail.co.uk>
2019-01-09 17:16:03 +0000

    added more graphs

--- graphs/views.py
+++ graphs/views.py
@@ -1,7 +1,8 @@
 import json
 from django.shortcuts import render
-from django.db.models import Count, Q
+from django.db.models import Count, Q, Sum
 from tickets.models import Ticket
+from donations.models import Donation
 
 
 def graphs(request):
@@ -17,30 +18,34 @@ def graphs(request):
         titles.append(entry.title)
         data.append(entry.upvotes)
     
-    chart = {
-        "chart": {"type": "column"},
-        "title": {"text": "Bug Tickets by upvote"},
+    upvote_chart = {
+        "chart": {"type": "bar"},
+        "title": {"text": "Bug Tickets By Upvote"},
+        "colors": ["#DADFE5", "#39393A", "#A5C1B7", "#A6B990", "#628193"],
         "plotOptions": {
             "series": {
                 "colorByPoint": "true"
             },
         },
-        "xAxis": {"categories": titles},
+        "xAxis": {
+            "categories": titles},
         "yAxis": {
             "min": 0,
             "title": {
                 "text": "No of Upvotes"
-            }
+            },
+            "tickInterval": 2
         },
         "series": [{ 
-            "name": "Tickets",
+            "name": "Upvotes",
             "data": data
         }]
     }
 
-    upvote_chart = json.dumps(chart)
+    upvote_chart = json.dumps(upvote_chart)
     
     
+    # Bug Tickets By Status
     ticket_status = Ticket.objects \
         .values("title") \
         .annotate(not_started_count=Count('title', filter=Q(status="Not Started")),
@@ -57,25 +62,67 @@ def graphs(request):
         done_count += (entry["done_count"])
    
     
-    chart = {
+    status_chart = {
         "chart": {"type": "pie"},
-        "title": {"text": "Bug Tickets by status "},
+        "title": {"text": "Bug Tickets By Status "},
         "series": [{
-            "name": 'Status',
+            "name": 'Statuses',
             "colorByPoint": "true",
             "data": [{ 
                 "name": "Not Started",
-                "y": not_started_count
+                "y": not_started_count,
+                "color": "#39393A"
             }, { 
                 "name": "Started",
-                "y": started_count
+                "y": started_count,
+                "color": "#628193"
             }, { 
                 "name": "Done",
-                "y": done_count
+                "y": done_count,
+                "color": "#A6B990"
             }]
         }]
     }
 
-    status_chart = json.dumps(chart)
+    status_chart = json.dumps(status_chart)
+    
+    
+    # Donation Amount By Date 
+    data = Donation.objects \
+        .values("date") \
+        .annotate(amount=Sum('donation_amount'))
 
-    return render(request, "graphs.html", {"upvote_chart": upvote_chart, "status_chart": status_chart})
+    donations = list()
+    
+    for entry in data:
+        donation = [(entry["date"].strftime("%Y-%m-%d")), entry["amount"] / 100]
+        donations.append(donation)
+    
+    donation_chart = {
+        "chart": {"type": "column"},
+        "title": {"text": "Donations"},
+        "xAxis": {
+            "type": "category",
+            "labels": {
+                "rotation": -45
+            },
+            "title": {
+                "text": 'Date'
+            }
+        },
+        "yAxis": {
+            "title": {
+                "text": "Amount in £s"
+            },
+            "min": 0
+        },
+        "series": [{
+            "name": 'Donations',
+            "color": "#628193",
+            "data": donations,
+        }]
+    }
+    
+    donation_chart = json.dumps(donation_chart)
+
+    return render(request, "graphs.html", {"upvote_chart": upvote_chart, "status_chart": status_chart, "donation_chart": donation_chart})
